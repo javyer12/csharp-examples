d5ee2fc75baed63cf2dab2d43e16b8c32263e347
Francisco Murillo <70788090+javyer12@users.noreply.github.com>
2022-12-20 19:15:35 +0000

    get country

--- myApp-e/Program.cs
+++ myApp-e/Program.cs
@@ -1,2 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("time is: " + DateTime.Now());
+﻿// string currentCountry = "Honduras";
+// Console.WriteLine("Insert your name and country");
+// string country = Console.ReadLine();
+// Console.WriteLine("Insert your name and name");
+// string name = Console.ReadLine();
+
+// void hiCountry(){
+//     Console.WriteLine("Hello, " + name + " We send you greeting from " + currentCountry + " to " + country);
+// }
+
+// hiCountry();
+
+void evaluateNumber(){
+    Console.WriteLine("Insert two numbers: ");
+    Console.WriteLine("Insert the first one: ");
+    int num1 = Console.ReadLine();
+    Console.WriteLine("Insert the Second one: ")
+    int num2 = Console.ReadLine();
+    Console.WriteLine(num1 + num2);
+}
+evaluateNumber()
